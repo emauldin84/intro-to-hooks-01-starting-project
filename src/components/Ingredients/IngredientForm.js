1d20a288024ca1471243ebc50957c5a1548caa37
Eric Mauldin <emauldin84@gmail.com>
2019-12-06 17:49:56 -0500

    seprate state into different useStates

--- src/components/Ingredients/IngredientForm.js
+++ src/components/Ingredients/IngredientForm.js
@@ -4,32 +4,35 @@ import Card from '../UI/Card';
 import './IngredientForm.css';
 
 const IngredientForm = React.memo(props => {
-  const [inputState, setInputState] = useState({
-    title: '',
-    amount: ''
-  })
+  const [inputTitle, setInputTitle] = useState('')
+  const [inputAmount, setInputAmount] = useState('')
+  
   const submitHandler = e => {
     e.preventDefault();
     // ...
   };
-  const changeHandler = e => {
-    setInputState({
-      ...inputState,
-      [e.target.id]: e.target.value
-    })
+  
+  const changeTitleHandler = e => {
+    setInputTitle(e.target.value)
   }
-  console.log(inputState)
+  
+  const changeAmountHandler = e => {
+    setInputAmount(e.target.value)
+  }
+  
+  console.log(inputTitle, inputAmount)
+  
   return (
     <section className="ingredient-form">
       <Card>
         <form onSubmit={submitHandler}>
           <div className="form-control">
             <label htmlFor="title">Name</label>
-            <input type="text" id="title" value={inputState.title} onChange={changeHandler}/>
+            <input type="text" id="title" value={inputTitle} onChange={changeTitleHandler}/>
           </div>
           <div className="form-control">
             <label htmlFor="amount">Amount</label>
-            <input type="number" id="amount" value={inputState.amount} onChange={changeHandler}/>
+            <input type="number" id="amount" value={inputAmount} onChange={changeAmountHandler}/>
           </div>
           <div className="ingredient-form__actions">
             <button type="submit">Add Ingredient</button>
