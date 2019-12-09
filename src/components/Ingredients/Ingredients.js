ee896c3662fb1e551bfedfb9e1fb963c70cb6ee4
Eric Mauldin <emauldin84@gmail.com>
2019-12-09 15:56:32 -0500

    adds filter functionality that makes new get request onChange, not ideal

--- src/components/Ingredients/Ingredients.js
+++ src/components/Ingredients/Ingredients.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 
 import IngredientForm from './IngredientForm';
 import IngredientList from './IngredientList'
@@ -7,23 +7,27 @@ import Search from './Search';
 const Ingredients = () => {
   const [ingredients, setIngredients] = useState([])
 
-  useEffect(() => {
-    fetch('https://react-hooks-starting-project.firebaseio.com/ingredients.json')
-      .then(response => response.json())
-      .then(responseData => {
-        console.log('useEffect responseData', responseData)
-        const loadedIngredients = []
-        for(let key in responseData){
-          loadedIngredients.push({
-            id: key,
-            title: responseData[key].title,
-            amount: responseData[key].amount
-          })
-        }
-        setIngredients([
-          ...loadedIngredients
-        ])
-      })
+  // useEffect(() => {
+  //   fetch('https://react-hooks-starting-project.firebaseio.com/ingredients.json')
+  //     .then(response => response.json())
+  //     .then(responseData => {
+  //       console.log('useEffect responseData', responseData)
+  //       const loadedIngredients = []
+  //       for(let key in responseData){
+  //         loadedIngredients.push({
+  //           id: key,
+  //           title: responseData[key].title,
+  //           amount: responseData[key].amount
+  //         })
+  //       }
+  //       setIngredients([
+  //         ...loadedIngredients
+  //       ])
+  //     })
+  // }, [])
+
+  const filteredIngredientsHandler = useCallback(filteredIngredients => {
+    setIngredients(filteredIngredients)
   }, [])
 
   const addIngredientHandler = ingredient => {
@@ -58,7 +62,7 @@ const Ingredients = () => {
       <IngredientForm addIngredient={addIngredientHandler}/>
 
       <section>
-        <Search />
+        <Search onLoadIngredients={filteredIngredientsHandler}/>
         <IngredientList ingredients={ingredients} removeItem={removeIngredientHandler}/>
       </section>
     </div>
