09013d6f23480416452b47abc56312df94b6d5f2
Eric Mauldin <emauldin84@gmail.com>
2019-12-09 14:08:29 -0500

    sets up firebase to store added ingredients. uses fetch to post to firebase

--- src/components/Ingredients/Ingredients.js
+++ src/components/Ingredients/Ingredients.js
@@ -8,13 +8,22 @@ const Ingredients = () => {
   const [ingredients, setIngredients] = useState([])
 
   const addIngredientHandler = ingredient => {
-    setIngredients([
-      ...ingredients,
-      {
-        id: Date.now(),
-        ...ingredient
-      }
-    ])
+    fetch('https://react-hooks-starting-project.firebaseio.com/ingredients.json',{
+      method: 'POST',
+      body: JSON.stringify(ingredient),
+      headers: { 'Content-Type': 'application/json' }
+    }).then(response => {
+      return response.json()
+    }).then(responseData => {
+      console.log(responseData)
+      setIngredients([
+        ...ingredients,
+        {
+          id: responseData.name,
+          ...ingredient
+        }
+      ])
+    })
   }
   const removeIngredientHandler = id => {
     let filteredIngredients = ingredients.filter(ingredient => {
